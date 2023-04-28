3bc6f2c6b6e2ccea5e7b88634262a7797efbf936
inwardik <inw@ukr.net>
2023-04-28 17:41:10 +0300

    Add tenses to menu

--- tki.py
+++ tki.py
@@ -1,12 +1,22 @@
 import random
 import tkinter as tk
 
-from content import sentences
+from content import sentences, sentences_tenses
+lesson_sentences = sentences
 
 
 class HTS:
 
     def __init__(self):
+
+        def set_sentences():
+            global lesson_sentences
+            lesson_sentences = sentences
+
+        def set_sentences_tenses():
+            global lesson_sentences
+            lesson_sentences = sentences_tenses
+
         self.window = tk.Tk()
         self.menubar = tk.Menu(self.window)
         self.empty_menu = tk.Menu(self.window)
@@ -17,9 +27,13 @@ class HTS:
         self.file_menu.add_separator()
         self.about_menu = tk.Menu(self.menubar, tearoff=0)
         self.about_menu.add_command(label="About", command=exit)
+        self.lessons_menu = tk.Menu(self.menubar, tearoff=0)
+        self.lessons_menu.add_command(label="basic", command=set_sentences)
+        self.lessons_menu.add_command(label="tenses", command=set_sentences_tenses)
 
         self.menubar.add_cascade(menu=self.file_menu, label="File")
         self.menubar.add_cascade(menu=self.about_menu, label="?")
+        self.menubar.add_cascade(menu=self.lessons_menu, label="Lessons")
 
         self.window.config(menu=self.empty_menu)
         self.is_menu_showed = False
@@ -37,13 +51,15 @@ class HTS:
         self.report_label = tk.Label(height=2, width=8, text="report", background=self.bg_color, fg=self.h_text_color)
         self.fav_label = tk.Label(height=2, width=8, text="favorite", background=self.bg_color, fg=self.h_text_color)
         self.en_label = tk.Label(height=4, width=43, background=self.bg_color, fg=self.text_color)
+        self.comment_label = tk.Label(height=4, width=43, background=self.bg_color, fg=self.text_color)
 
         def sentence_text(e):
             self.report_label["text"] = "report"
             self.fav_label["text"] = "favorite"
-            self.sentence = random.choice(sentences)
+            self.sentence = random.choice(lesson_sentences)
             self.ru_label["text"] = wrapped(self.sentence.get('ru'))
             self.en_label["text"] = "[     -     ]"
+            self.comment_label["text"] = ""
 
         def report_problem(e):
             self.report_label["text"] = "reported"
@@ -61,17 +77,22 @@ class HTS:
             self.report_label["text"] = "report"
             self.fav_label["text"] = "favorite"
             self.en_label["text"] = wrapped(self.sentence.get('en'))
+            self.comment_label["text"] = wrapped(self.sentence.get('comment'))
 
         def show_menu(e):
             menu = self.empty_menu if self.is_menu_showed else self.menubar
             self.is_menu_showed = not self.is_menu_showed
             self.window.config(menu=menu)
 
+        def set_lesson():
+            print('lesson')
+
         self.space_label.grid(row=0, column=0, columnspan=2)
         self.ru_label.grid(row=1, column=0, columnspan=2)
         self.report_label.grid(row=2, column=0)
         self.fav_label.grid(row=2, column=1)
         self.en_label.grid(row=3, column=0, columnspan=2)
+        self.comment_label.grid(row=4, column=0, columnspan=2)
 
         self.ru_label.bind("<Button-1>", sentence_text)
         self.report_label.bind("<Button-1>", report_problem)
